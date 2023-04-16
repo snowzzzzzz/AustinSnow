1242213c79794ec6d78db98d606d29e6a71b8d13
snow <wangsinuo.wsn@alibaba-inc.com>
2023-04-17 00:36:29 +0800

    1.接入springboot

---
+++ austin-web/src/main/java/com/snow/austin/AustinApplication.java
@@ -0,0 +1,14 @@
+package com.snow.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author snow
+ */
+@SpringBootApplication
+public class AustinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AustinApplication.class, args);
+    }
+}
---
+++ austin-web/src/main/java/com/snow/austin/web/TestController.java
@@ -0,0 +1,12 @@
+package com.snow.austin.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "first web, easy!";
+    }
+}
